30b48248572ae04c3f28f6d69779385da06cd2d2
faizaibrahim22 <faizaibrahim14789@gmail.com>
2024-09-18 22:53:07 +0500

    header

---
+++ app/components/About.tsx
@@ -0,0 +1,1 @@
+import lock from "../components/Header" ;
---
+++ app/components/Header.tsx
@@ -0,0 +1,8 @@
+function lock (){
+    return(
+        <>
+        <h1> hi  i am next.js</h1>
+        </>
+    )
+}
+export default lock;
